7b054ac0291a5362fc19a6e7b4d7f6820bae1329
Ricardo <parker1284@hotmail.com>
2021-07-06 19:22:55 -0500

    se crea objEvidencia sin propiedades y pseudocodigo para impeklemntar pantalla evidencias

--- app/src/main/java/com/nsidentity/tracking/CapturaEvidenciaActivity.java
+++ app/src/main/java/com/nsidentity/tracking/CapturaEvidenciaActivity.java
@@ -11,4 +11,26 @@ public class CapturaEvidenciaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_captura_evidencia);
     }
+
+
+    private void GuardarEnServidor()
+    {
+        //ArrayList<objEvidencia> lstEvidencias
+
+        //objEvidencia objE1= new objEvidencia();
+        //objE1.id=1
+        //objE1.foto=convertir imagen uno en base64 (debes de hacer pequeña la imagen width 400)
+        //objE1.nota=txtNota1.getText
+
+        //hjacer 2 veces mas
+
+        //implementar hilo para subir
+
+      //  for(lstEvidencias)
+            //subir 1 x 1
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/nsidentity/tracking/clases/objEvidencia.java
@@ -0,0 +1,9 @@
+package com.nsidentity.tracking.clases;
+
+public class objEvidencia {
+    //evidencia_id-int
+    //foto-string imagen en base 64
+    //nota string
+
+
+}
