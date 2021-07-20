45d3394ee77e816e7d2705b7daad2b8d0c5e9905
Diego <Diego-Ernesto_Alonso@hotmail.com>
2021-07-20 18:33:23 -0500

    Método pSalvaEvidencia

---
+++ app/src/main/java/com/nsidentity/tracking/soap/pSalvaEvidencia.java
@@ -0,0 +1,9 @@
+package com.nsidentity.tracking.soap;
+
+import com.nsidentity.tracking.basic.SendBase;
+
+public class pSalvaEvidencia extends SendBase<pSalvaEvidencia> {
+    protected int evidencia_id = 0;
+    protected String imagen = null;
+    protected String nota = null;
+}
